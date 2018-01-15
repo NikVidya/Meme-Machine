08d16c0d55e2e76b4873909f35b9bed31aacb72f
Unknown <n.mason1@outlook.com>
2018-01-15 02:11:20 -0800

    added regex youtube url check

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using RedditSharp;
 using Discord;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace memeMachine
 {
@@ -35,7 +36,7 @@ namespace memeMachine
             var subreddit = reddit.GetSubreddit("/r/youtubehaiku");
             foreach (var post in subreddit.Hot.GetListing(5))
             {
-                if (channel.Name.Equals("dankmachine"))
+                if (channel.Name.Equals("dankmachine") && isItAYoutube(post.Url.ToString()))
                 {
                     await channel.SendMessageAsync(post.Url.ToString());
                 }
@@ -52,6 +53,25 @@ namespace memeMachine
         {
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
+        }
+           private bool isItAYoutube(string matchme)
+        {
+            string pattern = "#^(?:https?://)?";    // Optional Url scheme. Either http or https
+            pattern += "(?:www\\.)?";               // Optional www subdomain
+            pattern += "(?:";                       // Hosts group:
+            pattern += "youtu\\.be/";               //   Either youtu.be, 
+            pattern += "|youtube\\.com";            //   or youtube.com 
+            pattern += "(?:";                       //   Paths group: 
+            pattern += "/embed/";                   //     Either /embed/, 
+            pattern += "|/v/";                      //	   or /v/,
+            pattern += "|/watch\\?v=";              //     or /watch?v=, 
+            pattern += "|/watch\\?.+&v=";           //     or /watch?other_param&v= 
+            pattern += ")";                         //   End paths group. 
+            pattern += ")";                         // End hosts group.
+            pattern += "([\\w-]{11})";              // 11 characters (Length of Youtube video ids).
+            pattern += "(?:.+)?$#x";	            // Optional other ending URL parameters.
+            var regex = new Regex(pattern);
+            return regex.IsMatch(matchme);
         }
     }
 }
